6a52e1402686d50b57df4c93574e2998346e4875
Rashaun Bennett <rashaunbennett1017@gmail.com>
2022-10-27 19:38:08 -0400

    front&back

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,31 @@
-console.log('Server...');
+const express = require('express');
+
+const colors = require('colors');
+
+const dotenv = require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+const { errorHandler } = require('./middleware/errorMiddleware');
+
+const connectDB = require('./config/db');
+
+// Connect to database
+connectDB();
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(PORT, () => console.log(`Server started on Port ${PORT}`));
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello Postman' });
+});
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'));
+
+app.use(errorHandler);
---
+++ frontend/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <div>MY APP</div>;
+}
+
+export default App;
