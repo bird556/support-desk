43fc7c3c01af75340cc936f7e72cac3e06dd8552
Rashaun Bennett <rashaunbennett1017@gmail.com>
2022-10-31 13:26:46 -0400

    fix2

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -2,11 +2,13 @@ import { Navigate, Outlet } from 'react-router-dom';
 import { useAuthStatus } from '../hooks/useAuthStatus';
 import Spinner from './Spinner';
 
-export const PrivateRoute = () => {
+function PrivateRoute() {
   const { loggedIn, checkingStatus } = useAuthStatus();
 
   if (checkingStatus) {
     return <Spinner />;
   }
   return loggedIn ? <Outlet /> : <Navigate to="/login" />;
-};
+}
+
+export default PrivateRoute;
